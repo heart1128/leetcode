9cef9c564015e9a4b951ab92f7514d37dddea2ed
reedleaf <1020273485@qq.com>
2019-04-02 14:43:04 +0800

    LeetCode_15,3Sum解法

---
+++ 3Sum/threeSum_Ksum通解.cpp
@@ -0,0 +1,89 @@
+﻿/*
+
+	3Sum(KSum) 
+	描述：
+	给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组。
+	注意：答案中不可以包含重复的三元组。
+	例如, 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+
+	满足要求的三元组集合为：
+	[
+	 [-1, 0, 1],
+	[-1, -1, 2]
+	]
+
+	解法：
+			 先排序数组，这样就有规律可寻，
+			 给定两个移动下标，一个当前可用最大，一个为当前可用最小,如果三数加起来大于0，那就是R太大，往小的移,Ld的相反
+			 如果判定到相等的,加入数组，然后最重要的去重，一直往下一个比较，如果相等就不用比较的，
+			 最后给最外层去重，同样的方法
+
+			 关于KSum的解，都是类似的方法
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <ctime>
+using namespace std;
+static const auto io_sync_off = []() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	return nullptr;
+}();
+
+class Solution {
+public:
+	vector<vector<int>> threeSum(vector<int>& nums) {
+		vector<vector<int>> v;
+		if (nums.size() < 3)
+			return v;
+
+		int len = nums.size();
+		sort(nums.begin(), nums.end());
+		for (int i = 0; i < len - 2; ++i)
+		{
+			if (nums[i] > 0)
+				break;
+			int L = i + 1;
+			int R = len - 1;
+			while (L < R)
+			{
+				if (nums[i] + nums[L] + nums[R] > 0)
+					R--;
+				else if (nums[i] + nums[L] + nums[R] < 0)
+					L++;
+				else
+				{
+					v.push_back({ nums[i],nums[L] ,nums[R]});
+					while (L+1 < len && nums[L] == nums[L + 1]) L++;
+					L++;
+				}
+			}
+			while (i+1 < len && nums[i] == nums[i + 1])
+				i++;
+		}
+		return v;
+	}
+};
+int main()
+{
+	clock_t begin, end;
+	begin = clock();
+	vector<int> v{2,41,45,5,5,0,4,0,4, -4,5,-1,-21,-4,-5,5,1,0};
+	Solution a;
+	auto it = a.threeSum(v);
+	for (auto array : it)
+	{
+		cout << "[";
+		for (auto value : array)
+		{
+			cout << value << ",";
+		}
+		cout << "]" << endl;
+	}
+	end = clock();
+	cout << "用时" << (begin - end) / CLOCKS_PER_SEC << "\n";
+	system("pause");
+	return 0;
+}
---
+++ 3Sum/threeSum_暴力解.cpp
@@ -0,0 +1,84 @@
+﻿/*
+
+		LeetCode 3Sum:
+		暴力超时代码
+
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+static const auto io_sync_off = []() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	return nullptr;
+}();
+
+class Solution {
+public:
+	vector<vector<int>> threeSum(vector<int>& nums) {
+		sort(nums.begin(), nums.end());
+		vector<vector<int>> v;
+		if (nums.size() < 3)
+			return v;
+		if (nums.size() == 3 && (nums[0] + nums[1] + nums[2]) != 0)
+			return v;
+		else if (nums.size() == 3 && nums[0] + nums[1] + nums[2] == 0)
+		{
+			v.push_back(nums);
+			return v;
+		}
+		
+		for (size_t i = 0; i < nums.size(); ++i)
+		{
+			for (size_t j = i + 1; j < nums.size(); ++j)
+			{
+					int n = 0 - nums[i] - nums[j];
+					vector<int> temp{ n,nums[i],nums[j] };
+					sort(temp.begin(), temp.end());
+					auto it = find(v.begin(), v.end(), temp);
+					if (middle(nums, n,i,j) && it == v.end())
+					{
+						v.push_back(temp);
+					}
+			}
+		}
+		return v;
+	}
+
+	bool middle(vector<int>& nums, int n,int i,int j)
+	{
+		int L = 0;
+		int R = nums.size() - 1;
+		while (L <= R)
+		{
+			int mid = (R + L) / 2;
+			if (mid != i && mid != j && n == nums[mid])
+				return true;
+			if (n > nums[mid])
+				L = mid + 1;
+			else
+				R = mid - 1;
+		}
+
+		return false;
+	}
+};
+int main()
+{
+	vector<int> v{10,-2,-12,3,-15,-12,2,-11,3,-12,9,12,0,-5,-4,-2,-7,-15,7,4,-5,-14,-15,-15,-4,10,9,-6,7,1,12,-6,14,-15,12,14,10,0,10,-10,3,4,-12,10,7,-9,-7,-15,-8,-15,-4,2,9,-4,3,-10,13,-3,-1,5,5,-4,-15,4,-11,4,-4,6,-11,-9,12,7,11,7,2,-5,13,10,-5,-10,3,7,0,-3,10,-12,2,9,-8,8,-9,13,12,13,-6,8,3,5,-9,7,12,10,-8,0,2,1,10,-7,-3,-10,-10,7,4,5,-11,-8,0,-2,-14,8,13,-8,-2,10,13 };
+	Solution a;
+	auto it = a.threeSum(v);
+	for (auto array : it)
+	{
+		cout << "[";
+		for (auto value : array)
+		{
+			cout << value << ",";
+		}
+		cout << "]" << endl;
+	}
+	system("pause");
+	return 0;
+}
